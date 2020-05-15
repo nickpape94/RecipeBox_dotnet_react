f14dd32e5966d6f7205149bd5da989bdb35450ec
nickpape94 <nick-_p@hotmail.com>
2020-05-15 19:22:27 +0100

    setting up test folder

---
+++ RecipeBox.API/src/Test/UnitTest1.cs
@@ -0,0 +1,13 @@
+using NUnit.Framework;
+
+namespace Test
+{
+    public class Tests
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.AreEqual(5, 6);
+        }
+    }
+}
