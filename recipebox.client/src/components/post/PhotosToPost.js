56b4a0f606092fd770bbe6438d7b2eb9c0ade651
nickpape94 <nick-_p@hotmail.com>
2020-09-09 17:23:54 +0100

    almost working

--- recipebox.client/src/components/post/PhotosToPost.js
+++ recipebox.client/src/components/post/PhotosToPost.js
@@ -5,29 +5,41 @@ import { connect } from 'react-redux';
 import { addRecipePhotos } from '../../actions/photo';
 import { getPost } from '../../actions/post';
 import { getUser } from '../../actions/user';
+import Spinner from '../layout/Spinner';
 
-const PhotosToPost = ({ getPost, post: { post }, match, user }) => {
-	useEffect(() => {
-		getPost(match.params.postId);
-	}, []);
+const PhotosToPost = ({ getPost, post: { post }, match, auth: { loading, isAuthenticated, user } }) => {
+	useEffect(
+		() => {
+			getPost(match.params.postId);
+		},
+		[ getPost ]
+	);
 
-	// console.log(post.postId);
-	// console.log(user);
-	// const idOfPost = post && post.userId;
-	// const idOfLoggedInUser = auth.user && auth.user.id;
+	const idOfPost = post && post.userId;
+	const idOfLoggedInUser = user && user.id;
 
-	// if (idOfPost && idOfLoggedInUser === null) {
-	// 	return <Redirect to={`/add-photos/${post.postId}`} />;
-	// }
+	if (loading) {
+		return <Spinner />;
+	}
+	if (idOfPost !== idOfLoggedInUser && isAuthenticated === true) {
+		console.log('id of post:' + idOfPost);
+		console.log('id of logged in user:' + idOfLoggedInUser);
+		console.log('loading status: ' + loading);
+		console.log('auth status: ' + isAuthenticated);
+		return <Redirect to={`/posts`} />;
+	}
 	// const id = [ post && post.userId, auth.user && auth.user.id ];
 	// console.log(id);
-	// console.log(idOfPost);
-	// console.log(idOfLoggedInUser);
-
+	console.log('id of post:' + idOfPost);
+	console.log('id of logged in user:' + idOfLoggedInUser);
+	console.log('loading status: ' + loading);
+	console.log('auth status: ' + isAuthenticated);
 	// {idOfPost !== idOfLoggedInUser && <Redirect to='/posts' />}
 	return (
 		<Fragment>
 			<h1>somethinf</h1>
+			<h1>{idOfPost}</h1>
+			<h1>{idOfLoggedInUser}</h1>
 			{/* {idOfPost !== idOfLoggedInUser && <Redirect to='/posts' />} */}
 		</Fragment>
 	);
