e8c49e379f21264ac3ffaf9dd7fb7926829a4db3
nickpape94 <nick-_p@hotmail.com>
2020-10-27 19:38:10 +0000

    started implementing cuisines component

---
+++ recipebox.client/src/components/posts/Cuisines.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import african from '../../img/cuisines/african.jpg';
+import american from '../../img/cuisines/american.jpg';
+import british from '../../img/cuisines/british.webp';
+import caribbean from '../../img/cuisines/caribbean.webp';
+import chinese from '../../img/cuisines/chinese.jpg';
+import easteuropean from '../../img/cuisines/easteuropean.jpg';
+import french from '../../img/cuisines/french.jpg';
+import greek from '../../img/cuisines/greek.jpg';
+import indian from '../../img/cuisines/indian.jpg';
+import italian from '../../img/cuisines/italian.jpg';
+import japanese from '../../img/cuisines/japanese.jpg';
+import korean from '../../img/cuisines/korean.jpg';
+import mexican from '../../img/cuisines/mexican.jpg';
+import middleeastern from '../../img/cuisines/middleeastern.jpg';
+import pakistani from '../../img/cuisines/pakistani.webp';
+import portuguese from '../../img/cuisines/portuguese.jpg';
+import southamerican from '../../img/cuisines/southamerican.jpg';
+import spanish from '../../img/cuisines/spanish.jpg';
+import thai from '../../img/cuisines/thai.jpg';
+import turkish from '../../img/cuisines/turkish.jpg';
+
+const Cuisines = (props) => {
+	return (
+		<div>
+			<img src={african} alt='' />
+		</div>
+	);
+};
+
+Cuisines.propTypes = {};
+
+export default Cuisines;
