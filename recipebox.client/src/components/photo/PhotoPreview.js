ebcd016d183d28b158c6b958b5160f8cf272013a
nickpape94 <nick-_p@hotmail.com>
2020-12-02 15:01:20 +0000

    almost got the functionality of photos working as intended

--- recipebox.client/src/components/photo/PhotoPreview.js
+++ recipebox.client/src/components/photo/PhotoPreview.js
@@ -13,6 +13,14 @@ import {
 } from '../layout/PhotoUploadStyles';
 
 const PhotoPreview = ({ files, setFiles, newFiles = [], setNewFiles, edit = false, deleteRecipePhoto }) => {
+	// useEffect(
+	// 	() => {
+	// 		// setNewFiles([...newFiles].filter((x) => x !== null));
+	// 		console.log('done');
+	// 	},
+	// 	[ newFiles ]
+	// );
+
 	const removeFile = (file) => () => {
 		const fileRemoved = [ ...files ];
 		fileRemoved.splice(fileRemoved.indexOf(file), 1);
@@ -25,10 +33,15 @@ const PhotoPreview = ({ files, setFiles, newFiles = [], setNewFiles, edit = fals
 	const removeNewFile = (file) => () => {
 		const newFileRemoved = [ ...newFiles ];
 		newFileRemoved.splice(newFileRemoved.indexOf(file), 1);
-		setFiles(newFileRemoved);
+		setNewFiles(newFileRemoved);
 	};
 
 	const removeAllFiles = () => {
+		// if (!edit) {
+		// 	setFiles([]);
+		// } else {
+		// 	setNewFiles([]);
+		// }
 		setFiles([]);
 	};
 
@@ -48,7 +61,7 @@ const PhotoPreview = ({ files, setFiles, newFiles = [], setNewFiles, edit = fals
 						)
 					)
 				: setNewFiles(
-						acceptedFiles.map((file) =>
+						acceptedFiles.slice(0, 6 - files.length).map((file) =>
 							Object.assign(file, {
 								preview: URL.createObjectURL(file)
 							})
@@ -58,8 +71,9 @@ const PhotoPreview = ({ files, setFiles, newFiles = [], setNewFiles, edit = fals
 	});
 
 	files.length = Math.min(files.length, 6);
-	newFiles.length = 6 - files.length;
-	console.log(files.length, newFiles.length);
+	// newFiles.length = 6 - files.length;
+	// newFiles.length = Math.min(6 - newFiles.length, 6);
+	// console.log(files.length, newFiles.length);
 
 	const style = useMemo(
 		() => ({
@@ -90,11 +104,7 @@ const PhotoPreview = ({ files, setFiles, newFiles = [], setNewFiles, edit = fals
 	const newThumbs = newFiles.map((file) => (
 		<div className='thumb-style' style={thumb} key={file.name}>
 			<div style={thumbInner}>
-				{files[0].postPhotoId !== undefined && files[0].postPhotoId !== null ? (
-					<img src={file.url} style={img} />
-				) : (
-					<img src={file.preview} style={img} />
-				)}
+				<img src={file.preview} style={img} />
 
 				<button onClick={removeNewFile(file)}>
 					<i className='fas fa-trash-alt fa-2x' />
@@ -115,6 +125,7 @@ const PhotoPreview = ({ files, setFiles, newFiles = [], setNewFiles, edit = fals
 	return (
 		<div className='my-2 text-center'>
 			<section className='container'>
+				{/* Dealing with preview of new files from a new post and not yet persisted to cloudinary  */}
 				{!edit && (
 					<div {...getRootProps({ style })}>
 						<input {...getInputProps()} />
@@ -123,6 +134,7 @@ const PhotoPreview = ({ files, setFiles, newFiles = [], setNewFiles, edit = fals
 						<button className='button my-1 btn btn-primary'>Open Files</button>
 					</div>
 				)}
+				{!edit && files.length > 0 && <aside style={thumbsContainer}>{thumbs}</aside>}
 
 				{files.length === 0 && edit && <h3>No photos currently uploaded</h3>}
 				{files.length > 0 &&
@@ -132,15 +144,9 @@ const PhotoPreview = ({ files, setFiles, newFiles = [], setNewFiles, edit = fals
 						<aside style={thumbsContainer}>{thumbs}</aside>
 					</Fragment>
 				)}
-				{!edit && (
+				{!edit &&
+				files.length > 0 && (
 					<button className='btn btn-danger' onClick={removeAllFiles}>
-						Remove All
-					</button>
-				)}
-				{newFiles.length > 0 &&
-				newFiles[0] != null &&
-				edit && (
-					<button className='btn btn-danger' onClick={removeAllNewFiles}>
 						Remove All
 					</button>
 				)}
@@ -150,10 +156,19 @@ const PhotoPreview = ({ files, setFiles, newFiles = [], setNewFiles, edit = fals
 						<input {...getInputProps()} />
 						<p>Drag 'n' drop some files here</p>
 						<p>(Maximum of 6 photos)</p>
-						<button className='button my-1 btn btn-primary'>Open Files</button>
+						<button onClick={() => setNewFiles([])} className='button my-1 btn btn-primary'>
+							Open Files
+						</button>
 					</div>
 				)}
-				{edit && newFiles.length === 0 && <aside style={thumbsContainer}>{newThumbs}</aside>}
+				{newFiles.length > 0 &&
+				newFiles[0] != null &&
+				edit && (
+					<button className='btn btn-danger' onClick={removeAllNewFiles}>
+						Remove All
+					</button>
+				)}
+				{edit && newFiles.length > 0 && <aside style={thumbsContainer}>{newThumbs}</aside>}
 			</section>
 		</div>
 	);
